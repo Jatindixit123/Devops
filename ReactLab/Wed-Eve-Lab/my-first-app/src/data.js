968330b6d2186094af9264cb4e105affb403cd98
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-01-16 10:41:28 +0530

    Add files via upload

---
+++ ReactLab/Wed-Eve-Lab/my-first-app/src/data.js
@@ -0,0 +1,17 @@
+export const USER_DATA = [
+    {
+        username: "Virat Kohli",
+        userAge: 39,
+        profilePic: "https://www.hindustantimes.com/static-content/1y/cricket-logos/players/virat-kohli.png"
+    },
+    {
+        username: "Rohit Sharma",
+        userAge: 38,
+        profilePic: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Prime_Minister_Of_Bharat_Shri_Narendra_Damodardas_Modi_with_Shri_Rohit_Gurunath_Sharma_%28Cropped%29.jpg"
+    },
+    {
+        username: "MS Dhoni",
+        userAge: 44,
+        profilePic: "https://i.pinimg.com/736x/67/89/2f/67892f6f0980bee446f01794a5aba8f3.jpg"
+    },
+]
