96e9fe3bc238965c22040bda268243b897736a0e
sqb-codes <150049400+sqb-codes@users.noreply.github.com>
2025-01-26 10:46:50 +0530

    Add files via upload

---
+++ ReactLab/Tue-Noon-Lab/my-second-app/src/data.js
@@ -0,0 +1,20 @@
+export const USER_DATA = [
+    {
+        id: 101,
+        userName: "Virat Kohli",
+        userAge: 39,
+        profilePic: "https://documents.iplt20.com/ipl/IPLHeadshot2024/2.png"
+    },
+    {
+        id: 102,
+        userName: "Rohit Sharma",
+        userAge: 38,
+        profilePic: "https://upload.wikimedia.org/wikipedia/commons/1/1e/Prime_Minister_Of_Bharat_Shri_Narendra_Damodardas_Modi_with_Shri_Rohit_Gurunath_Sharma_%28Cropped%29.jpg"
+    },
+    {
+        id: 103,
+        userName: "MS Dhoni",
+        userAge: 44,
+        profilePic: "https://img.etimg.com/thumb/width-1200,height-1200,imgsize-21598,resizemode-75,msid-114813372/news/sports/csk-ipl-2025-retained-players-full-list-of-players-and-cost-retained-by-chennai-super-kings-ms-dhoni-ruturaj-gaikwad-ravindra-jadeja.jpg"
+    }
+]
